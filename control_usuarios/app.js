29c0b1edd51431cc7ac0b0ef0690a41eb123723d
Ana Karen Baltazar Cabrera <cabrerak454@gmail.com>
2024-10-07 12:02:12 -0600

    Modificación/agregar al proyecto control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -2,23 +2,42 @@ const express = require ('express');
 
 const app = express()
 
+const usuarios = [
+    {
+        id: 1,
+        nombre: "Juan",
+        apellido: "Perez",
+        email: "juanperez@gmail.com"
+    },
+    {
+        id: 2,
+        nombre: "Pepe",
+        apellido: "Martinez",
+        email: "pepemartinez@gmail.com"
+    }
+]
+
 app.get("/usuarios", (req, res) => {
-    const usuarios = [
-        {
-            id: 1,
-            nombre: "Juan",
-            apellido: "Perez",
-            email: "juanperez@gmail.com"
-        },
-        {
-            id: 2,
-            nombre: "Pepe",
-            apellido: "Martinez",
-            email: "pepemartinez@gmail.com"
-        }
-    ]
-    res.status(200).send(usuarios);
+
+    res.status(200).send({usuarios});
 })
+//para solo visualizar un usuario
+// app.get("/usuarios/1", (req, res) => {
+//     res.status(200).send(usuarios[0]);
+// });
+//solo imprime el primero, con la palabra id logramos reducir
+//lineas de codigo, y solamente nos imprime el que le indicamos 
+//en el arreglo.
+
+//find --- endpoints
+app.get("/usuarios/:id", (req, res) => {
+    const {id} = req.params;
+    
+    const usuario = usuarios.find((usuario) => usuario.id === +id);
+    res.status(200).send(usuario);
+});
+
+
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en http://localhost:3000/");
