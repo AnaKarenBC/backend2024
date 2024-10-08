6d558206c91bfb1fdc06cfb86986b6e88088c8f7
Ana Karen Baltazar Cabrera <cabrerak454@gmail.com>
2024-10-08 11:58:04 -0600

    Modificación al proyecto control_usuarios

--- control_usuarios/app.js
+++ control_usuarios/app.js
@@ -29,11 +29,23 @@ app.get("/usuarios", (req, res) => {
 //lineas de codigo, y solamente nos imprime el que le indicamos 
 //en el arreglo.
 
-//find --- endpoints
+//find --- end points
 app.get("/usuarios/:id", (req, res) => {
     const {id} = req.params;
-    
+    //console.log(typeof id); // me dice que es un string
+    //cuando se coloca el simbolo + es para comvertirlo en numero
+    //console.log(typeof +id) // me dice que es un numero
+    if(isNaN(id)){
+        res.status(400).send({error: "El id debe de ser un número"});
+        return;
+    } 
+
     const usuario = usuarios.find((usuario) => usuario.id === +id);
+    if(usuario === undefined){
+        //res.status(400).send({error: "Usuario no encontrado"}); //mensaje sencillo
+        res.status(400).send({error: `El usuario con id ${id} no existe`})
+        return;
+    }
     res.status(200).send(usuario);
 });
 
