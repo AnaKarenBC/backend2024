42d79e8abcf0e41783a092a0c2072e3ba5ed2c75
Ana Karen Baltazar Cabrera <cabrerak454@gmail.com>
2024-10-30 11:11:55 -0600

    Archivos nuevos al proyecto stock_manager

--- stock_manager/src/controllers/users.js
+++ stock_manager/src/controllers/users.js
@@ -1,7 +1,79 @@
 const {request, response} = require('express');
 
-const getMessage = (req = request, res = response) => {
-    res.send('Hello from the users controller!');
-};
+const users = [
+        {id: 1, name: 'John'},
+        {id: 2, name: 'Jane'},
+        {id: 3, name: 'Peter'},
+];
 
-module.exports = {getMessage};
+const getAll = (req=request, res=response) => {
+        res.send(users);
+}
+
+const getById = (req=request, res=response) => {
+        const {id} = req.params;
+        
+        if(isNaN(id)) {
+            res.status(400).json({msg: 'Invalid ID'});
+            return;
+        }
+
+        const user = users.find(user => user.id === +id);
+
+        if(!user) {
+            res.status(404).json({msg: 'User not found bruh'});
+            return;
+        }
+
+        res.send(user);
+}
+
+// hacer enpoints para crear, borrar y editar usuarios 
+//Agregar nuevo usuario
+const createUser = (req=request, res=response) => {
+    const {name} = req.body;
+
+    if(!name){
+        res.status(400).send("Bad request. The Name field is missing.");
+        return;
+    }
+    const user = users.find(user => user.name === name)
+
+    if(user){
+        res.status(409).send('user already existis');
+        return;
+    }
+
+    user.push({id: users.length +1, name});
+    res.send("User created succesfully");
+}
+
+//Actualizar usuario
+const updateUser = (req=request, res=response) => {
+
+    if(isNaN(id)) {
+        res.status(400).json({msg: 'Invalid ID'});
+        return;
+    }
+
+    const user = users.find(user => user.id === +id);
+
+    if(!user) {
+        res.status(404).json({msg: 'User not found bruh'});
+        return;
+    }
+
+    user.forEach(user => {
+        if(user.id === +id){
+            user.name = name;
+        }
+    });
+    res.send("User Updated succesfuly");
+}
+
+//Eliminar un usuario
+const deleteUser = (req=request, res=response) => {
+    
+}
+
+module.exports = {getAll, getById, createUser, updateUser};
--- stock_manager/src/routes/users.js
+++ stock_manager/src/routes/users.js
@@ -1,8 +1,15 @@
 const {Router} = require('express');
-const {getMessage} = require('../controllers/users');
+const {getAll, getById, createUser, updateUser} = require('../controllers/users');
 
 const router = Router();
 
-router.get('/', getMessage);
+router.get('/', getAll);
+router.get('/:id', getById);
+
+//Agregar un usuario nuevo
+router.post('/', createUser);
+//Actualizar un usuario nuevo
+router.put('/:id', updateUser);
+//Eliminar un usuario nuevo
 
 module.exports = router;
